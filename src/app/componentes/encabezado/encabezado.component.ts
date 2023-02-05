7d64c09e5ac6ec1be229f98f0473e9a13bb54e30
migueprograma34 <mr141434@gmail.com>
2023-02-05 00:42:08 -0300

    ++

--- src/app/componentes/encabezado/encabezado.component.ts
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-encabezado',
   templateUrl: './encabezado.component.html',
   styleUrls: ['./encabezado.component.css']
-})
-export class EncabezadoComponent {
-
+ })
+ export class EncabezadoComponent {
+ persona: persona = new persona("", "","");
+ constructor(public personaService: PersonaService){
+  this.personaService.getPersona().subscribe(data => {this.persona = data})
+ }
 }
